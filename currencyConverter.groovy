b2a3f2dfb2d5ec2881b0e8d9c655dec066e674e8
Sergio Gutierrez-Santos "Sergut <sergut@gmail.com>
2012-07-26 16:17:12 +0100

    Minor bug fixes:   - Added a space in println so the program's output looks nicer.   - Added empty line inside loop so the program's output looks nicer.

--- currencyConverter.groovy
+++ currencyConverter.groovy
@@ -1,13 +1,13 @@
 print "If I give you £1, how many Euro will you give me? ";
 String str = System.console().readLine();
 double euroOverPoundRatio = Double.parseDouble(str);
-print "If I give you 1€, how many Sterling pounds will you give me?";
+print "If I give you 1€, how many Sterling pounds will you give me? ";
 str = System.console().readLine();
 double poundOverEuroRatio = Double.parseDouble(str);
 println "OK, now I know enough.";
-println "";
 boolean finished = false;
 while (!finished) {
+    println "";
     println "What would you like to do?";
     println "   1 - Exchange pounds into euro";
     println "   2 - Exchange euro into pounds";

205cf8bfc35ca787fcb992d4969945580a2d843f
Sergio Gutierrez-Santos "Sergut <sergut@gmail.com>
2012-07-26 16:10:38 +0100

    Added the second ratio.

--- currencyConverter.groovy
+++ currencyConverter.groovy
@@ -1,4 +1,7 @@
 print "If I give you £1, how many Euro will you give me? ";
 String str = System.console().readLine();
 double euroOverPoundRatio = Double.parseDouble(str);
+print "If I give you 1€, how many Sterling poinds will you give me?";
+str = System.console().readLine();
+double poundOverEuroRatio = Double.parseDouble(str);
 

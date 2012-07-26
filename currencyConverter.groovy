e6c55270276ea69a3854d0b188a5f00887655535
Sergio Gutierrez-Santos "Sergut <sergut@gmail.com>
2012-07-26 16:07:51 +0100

    Better not to use println here, so that the cursor stays on the same line.

--- currencyConverter.groovy
+++ currencyConverter.groovy
@@ -1,3 +1,3 @@
-println "If I give you £1, how many Euro will you give me? ";
+print "If I give you £1, how many Euro will you give me? ";
 String str = System.console().readLine();
 double d = Double.parseDouble(str);
